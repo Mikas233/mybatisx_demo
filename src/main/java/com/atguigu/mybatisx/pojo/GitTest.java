c53bfa2a5e02bc316fe956897e08687ebb8ab432
Mikas <1260711582@qq.com>
2022-08-05 15:42:20 +0800

    Master Commit 02

--- src/main/java/com/atguigu/mybatisx/pojo/GitTest.java
+++ src/main/java/com/atguigu/mybatisx/pojo/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("GitTest -03");
 
         System.out.println("GitTest Hot-Fix");
+        System.out.println("GitTest master");
+        System.out.println("GitTest master");
     }
 }
