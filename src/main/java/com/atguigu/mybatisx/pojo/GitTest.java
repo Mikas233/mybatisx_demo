a96d7ba8aac043bff30da51876632ae2e6eafc4e
Mikas <1260711582@qq.com>
2022-08-05 14:31:55 +0800

    Second Commit

--- src/main/java/com/atguigu/mybatisx/pojo/GitTest.java
+++ src/main/java/com/atguigu/mybatisx/pojo/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.mybatisx.pojo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("GitTest -01");
+
+        System.out.println("GitTest -02");
     }
 }
